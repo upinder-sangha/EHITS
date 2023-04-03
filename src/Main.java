0b7cc271660be5b33128289c12429d859cee8878
karandeep7 <karansaini591@gmail.com>
2023-04-03 17:38:47 -0400

    Sorted aray complete

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        AVLTree newTree = new AVLTree();
+//        AVLTree newTree = new AVLTree();
+//        newTree.insert(1,"a");
+//        newTree.insert(2,"b");
+//        newTree.insert(3,"c");
+//        newTree.insert(4,"d");
+//        newTree.insert(5,"e");
+//        newTree.insert(5,"f");
+//        newTree.insert(6,"g");
+//        newTree.insert(7,"h");
+//        newTree.insert(8,"i");
+//        newTree.insert(9,"j");
+////        newTree.inorder();
+//        System.out.println(newTree);
+
+        SortedArray newTree = new SortedArray(4);
         newTree.insert(1,"a");
         newTree.insert(2,"b");
         newTree.insert(3,"c");
--- src/SortedArray.java
+++ src/SortedArray.java
@@ -1,2 +1,59 @@
 public class SortedArray {
+
+    public RecordList[] sortedArray;
+    private int maxCapacity;
+    private int currentNoOfElements;
+
+    public SortedArray(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        currentNoOfElements = 0;
+        sortedArray = new RecordList[this.maxCapacity];
+    }
+
+    private RecordList[] expand() {
+        RecordList[] newArray = new RecordList[maxCapacity * 2];
+        for (int i = 0; i < currentNoOfElements; i++) {
+            newArray[i] = sortedArray[i];
+        }
+        maxCapacity = newArray.length;
+        return newArray;
+    }
+
+    public void insert(int key, String value) {
+        if (currentNoOfElements == 0) {
+            sortedArray[0] = new RecordList(key, value);
+        } else {
+            if (maxCapacity == currentNoOfElements) {
+                this.sortedArray = expand();
+            }
+
+            int i = currentNoOfElements - 1;
+            while ((i >= 0)) {
+                if (sortedArray[i].compareTo(key) == 0) {
+                    sortedArray[i].addToStart(value);
+                    return;
+                }
+                i--;
+            }
+
+            i = currentNoOfElements - 1;
+            while ((i >= 0) && (sortedArray[i].compareTo(key) > 0)) {
+                sortedArray[i + 1] = sortedArray[i];
+                i--;
+            }
+
+            sortedArray[i + 1] = new RecordList(key, value);
+        }
+        currentNoOfElements++;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < currentNoOfElements; i++) {
+            sb.append(sortedArray[i] + ", ");
+        }
+        String str = sb.toString();
+        str = str.substring(0, (str.length() - 2));
+        return str;
+    }
 }
